69e1cee508bec9c2a9252c5b23257cbe41bcd872
Signsss <105465711+Signsss@users.noreply.github.com>
2022-06-23 07:07:55 +0100

    Add files via upload

---
+++ main.js
@@ -0,0 +1,16 @@
+var opr = prompt("Operator: ");
+var num1 = prompt("Number1: ");
+var num2 = prompt("Number2: ");
+
+if(opr == "+") {
+    console.log(num1 + num2)
+}
+else if(opr == "-"){
+    console.log(num1 - num2)
+}
+else if(opr == "*"){
+    console.log(num1 * num2)
+}
+else if(opr == "/"){
+    console.log(num1 / num2)
+}
